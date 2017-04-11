b2e5109ccbcd5411f224528c48fc9a137dbcc130
Lucas Guimaraes <lcascosta@hotmail.com>
2017-04-11 12:57:06 -0300

    Task Manager CRUD implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  resources :tasks
+
+  # get    "restaurants",          to: "restaurants#index"
+
+  # get    "restaurants/:id",      to: "restaurants#show"
+
+  # get    "restaurants/new",      to: "restaurants#new"
+  # post   "restaurants",          to: "restaurants#create"
+
+  # get    "restaurants/:id/edit", to: "restaurants#edit"
+  # patch  "restaurants/:id",      to: "restaurants#update"
+
+  # delete "restaurants/:id",      to: "restaurants#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
